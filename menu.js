972e0a52e05532f8ef1083f20fd2d853aeb1c2b6
Fran <franbl98@gmail.com>
2021-11-11 12:31:50 +0100

    Terminado loader

--- menu.js
+++ menu.js
@@ -1,3 +1,12 @@
+
+window.addEventListener("load", ()=>{
+    setTimeout(function(){
+        document.querySelector("#loaderContenedor").classList.toggle("hidden");
+    },3000)
+    document.querySelector("body").classList.toggle("hiddenFlow");
+    console.log(window.visualViewport);
+})
+
 let icono = document.querySelector(".icono");
 let ul = document.querySelector("ul");
 icono.addEventListener("click", (e)=>{
